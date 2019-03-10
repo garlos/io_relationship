c3287533a19a1b3530a7bf992a5c24122102c634
Deluxe-PC <wahid.hosseinzadeh@gmail.com>
2019-03-10 18:35:42 +0330

    rename class

---
+++ src/main/java/DoubleChangeListener.java
@@ -0,0 +1,10 @@
+//import javafx.beans.value.ChangeListener;
+//import javafx.beans.value.ObservableValue;
+//
+//public class DoubleChangeListener implements ChangeListener<Number> {
+//    @Override
+//    public void changed(ObservableValue<? extends Number> ov, Number oldValue, Number newValue)
+//    {
+//        System.out.println("the value has changed from " + oldValue + " to " + newValue);
+//    }
+//}
--- src/main/java/IORelationFinder.java
+++ src/main/java/IORelationFinder.java
@@ -1,8 +1,10 @@
+
+
 import java.util.*;
 
 public class IORelationFinder {
     private List<List<String>> content;
-    private List<String> tmpContent;
+    private ArrayList<String> tmpContent = new ArrayList<>(content.size());
 
 
     public void ioRelFinder(List<List<String>> content) {
@@ -16,67 +18,36 @@ public class IORelationFinder {
 
     public void extractSpecifiedRecords(List<List<String>> content) {
 
-        Map<Integer, String> hashMap = new HashMap<>();
         int count = 0;
-        String str = content.get(1).get(2).trim() +
-                content.get(1).get(3).trim() +
-                content.get(1).get(4).trim();
-        for (int i = 1; i < content.size(); i++) {
-            String tempStr = content.get(i).get(2).trim() +
-                    content.get(i).get(3).trim() +
-                    content.get(i).get(4).trim();
-            if (str.equals(tempStr)) {
-                System.out.println(content.get(i).get(0));
-//                        content.get(i).get(1) +
-//                        content.get(i).get(2) +
-//                        content.get(i).get(3) +
-//                        content.get(i).get(4));
-
+        for (int j = 1; j < content.size(); j++) {
+            String str = content.get(j).get(2).trim() +
+                    content.get(j).get(3).trim() +
+                    content.get(j).get(4).trim();
+            for (int i = j; i < content.size(); i++) {
+                String tempStr = content.get(i).get(2).trim() +
+                        content.get(i).get(3).trim() +
+                        content.get(i).get(4).trim();
+                String x1 = content.get(i).get(1);
+                String out1 = content.get(i).get(5);
+                if (str.equals(tempStr)) {
+                    tmpContent.add(content.get(i).get(0));
+                    System.out.format("No.%s:  ", tmpContent.get(count));
+                    System.out.format("%s - ", content.get(i).get(2).trim());
+                    System.out.format("%s - ", content.get(i).get(3).trim());
+                    System.out.format("%s\n", content.get(i).get(4).trim());
+                    System.out.println("------------------------");
+                    count++;
+                }
             }
-//            if (toString().equals())
-//            hashMap.put(Integer.parseInt(content.get(i).get(0)), content.get(i).get(2).trim() + content.get(i).get(3).trim() + content.get(i).get(4).trim());
+            System.out.println("similare counts: " + count);
         }
-//        System.out.println(hashMap.get(1));
-
-//        for (int i = 0; i < hashMap.size(); i++) {
-//            if (hashMap.get(1).equals(hashMap.get(i))) {
-//                tmpContent.add(content.get(i).get(0));
-//                System.out.println(i);
-//
-//            }
-//        }
-//
-//        for (int i = 1; i < tmpContent.size(); i++) {
-//            System.out.println(tmpContent.get(i));
-//        }
     }
-
-//        System.out.format("hash count: %d", hashMap.size());
-//        Scanner scan = new Scanner(System.in);
-//        scan.next();
-//
-
-
-    //        for (Map.Entry<String, String> entry : hashMap.entrySet()) {
-//            System.out.println(entry.getKey() + " = " + entry.getValue());
-//        }
-
-
-//        for (String str : strings) {
-//            if (counts.containsKey(str)) {
-//                counts.put(str, counts.get(str) + 1);
-//            } else {
-//                counts.put(str, "1");
-//            }
-//        }
-
-
-}
 
 
 //    public void relout1_in1() {
-//        for (int j = 1; j < content.size() - 1; j++) {
+//        int count=0;
 //
+//        for (int j = 1; j < content.size() - 1; j++) {
 //            System.out.format("period: %d\n", j);
 //            for (int i = 1; i < content.size() - 1; i++) {
 //                if ((!content.get(j).get(1).equals(content.get(j + 1).get(1))) &&
@@ -88,6 +59,6 @@ public class IORelationFinder {
 //                }
 //            }
 //        }
-//        System.out.format("count: %d", count);
+//        System.out.format("count: %d",count );
 //    }
-
+}
