9f679337b6c2ad0b67f418617bafb0da34c65274
Deluxe-PC <wahid.hosseinzadeh@gmail.com>
2019-04-20 21:08:28 +0430

    reducer completed- version 02

--- src/main/java/RF02_DataSetReducer.java
+++ src/main/java/RF02_DataSetReducer.java
@@ -2,7 +2,6 @@ import SUT.TestCase;
 import Utils.CsvOperation;
 import Utils.ReflectionUtils;
 import com.rits.cloning.Cloner;
-
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.*;
@@ -14,7 +13,6 @@ public class RF02_DataSetReducer {
                                                 Map<Integer, Integer> noRelfields,
                                                 List<ArrayList<Integer>> inOutFieldsIndex)
             throws IllegalAccessException, IOException {
-
 
         ArrayList<Integer> finalTestIdList = new ArrayList<>();
 
@@ -32,7 +30,7 @@ public class RF02_DataSetReducer {
                 if (!finalTestIdList.contains(remainedTestId)) {
 
                     finalTestIdList.add(remainedTestId);
-                    System.out.println("remained testId: " + remainedTestId);
+                    System.out.println("Remained Test Id: " + remainedTestId);
 
                 }
             }
@@ -40,8 +38,12 @@ public class RF02_DataSetReducer {
             tempDataSet.clear();
         }
 
-        System.out.println("final list size: " + finalTestIdList.size());
+        System.out.println("Size of Final Test Suites List: " + finalTestIdList.size());
+
         retrievedRemainedTestSuitesDataSet(dataSet, finalTestIdList);
+
+        System.out.println("Size of Final DataSet: " + dataSet.size());
+
         CsvOperation.saveDataSetToCSV(dataSet, 1, CsvOperation.csvHeadersGenerator(TestCase.class));
     }
 
@@ -57,7 +59,6 @@ public class RF02_DataSetReducer {
             StringBuilder inputValues = new StringBuilder();
             AllFieldsStringValue = ReflectionUtils.getAllFieldsStringValue(dataSet.get(i));
 
-
             for (int j = 0; j < inOutFieldsIndex.get(0).size(); j++) {
 
                 inputValues.append(AllFieldsStringValue.get(inOutFieldsIndex.get(0).get(j))).append(",");
@@ -67,10 +68,11 @@ public class RF02_DataSetReducer {
 
                 newList.add(inputValues.toString());
                 remainedTestId.add(dataSet.get(i).getTestId());
+
             }
         }
 
-        System.out.println("new list size: " + newList.size());
+        System.out.println("Size of Remained Test Suites: " + newList.size());
         return remainedTestId;
     }
 
@@ -79,13 +81,13 @@ public class RF02_DataSetReducer {
             throws IllegalAccessException {
 
         TestCase testCase = new TestCase();
-
         List<Field> fields = Utils.ReflectionUtils.getAllFields(testCase.getClass());
         Object firstRecordValues = ReflectionUtils.getField(dataSet.get(0), fields.get(noRelInputColumn));
 
         for (TestCase tCase : dataSet) {
 
             ReflectionUtils.setField(fields.get(noRelInputColumn), tCase, firstRecordValues);
+
         }
 
         return dataSet;
@@ -93,13 +95,23 @@ public class RF02_DataSetReducer {
 
     public static void retrievedRemainedTestSuitesDataSet(ArrayList<TestCase> dataSet, ArrayList<Integer> remainedTestIds) {
 
+        ArrayList<Integer> redundantTestSuites = new ArrayList<>();
 
         for (int i = 0; i < dataSet.size(); i++) {
 
-            if (!remainedTestIds.contains(dataSet.get(i).getTestId())) {
-                dataSet.remove(dataSet.get(i).getTestId());
-            }
+            redundantTestSuites.add(dataSet.get(i).getTestId());
         }
+
+        redundantTestSuites.removeAll(remainedTestIds);
+        System.out.println("Size of Redundant Test Suites: " + redundantTestSuites.size());
+        int i = 0;
+
+        while (i < dataSet.size()) {
+            if (redundantTestSuites.contains(dataSet.get(i).getTestId()))
+                dataSet.remove(i--);
+            i++;
+        }
+
     }
 }
 
--- src/test/java/tempRunner.java
+++ src/test/java/tempRunner.java
@@ -11,19 +11,19 @@ public class tempRunner {
 
         TestCase testCase = new TestCase();
 
-        ArrayList<TestCase> testSuitesArr = DS02_InputGenerator.generateInputs();
-
-        ArrayList<TestCase> dataSet = DS03_DataSetGenerator.generateDataSet(testSuitesArr);
 
         List<ArrayList<Integer>> inOutFieldsIndex = DS01_InOutRecognizer.recognizeInOutFields(testCase);
 //        System.out.println(inOutFieldsIndex.get(0));
 //        System.out.println(inOutFieldsIndex.get(1));
 
+        ArrayList<TestCase> testSuitesArr = DS02_InputGenerator.generateInputs();
+
+        ArrayList<TestCase> dataSet = DS03_DataSetGenerator.generateDataSet(testSuitesArr);
+
 
         Map<Integer, Integer> noRelfields = RF01_IORelationFinder.check_IO_Relations(testCase, inOutFieldsIndex, dataSet);
 
 //        System.out.println(Collections.singletonList(noRelfields));
-//
 //        System.out.println(noRelfields.keySet());
 
 
